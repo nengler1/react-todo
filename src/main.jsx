38fc765d9e865f74716a6734fcba34da502a3212
nengler <nengler@uccs.edu>
2025-10-30 14:03:34 -0600

    changed compontent IDs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { nanoid } from "nanoid";
 import './index.css'
 //import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.jsx'
 
 const DATA = [
-  { id: 'todo-0', name: 'Eat', completed: true },
-  { id: 'todo-1', name: 'Sleep', completed: false },
-  { id: 'todo-2', name: 'Fart', completed: false }
+  { id: `todo-${nanoid()}`, name: 'Eat', completed: false },
+  { id: `todo-${nanoid()}`, name: 'Sleep', completed: false },
+  { id: `todo-${nanoid()}`, name: 'Fart', completed: false }
 ];
 
 createRoot(document.getElementById('root')).render(
