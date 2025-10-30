24f232ee98050ce985993333b348c983e2994a43
nengler <nengler@uccs.edu>
2025-10-30 09:16:26 -0600

    Init React Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App(props) {
+  console.log(props);
+  return (
+    <>
+      <header>
+        <h1>Hello, {props.subject}</h1>
+        <button type='button' className='primary'>Click me!</button>
+      </header>
+    </>
+  );
+}
+
+export default App;
